f136e1e42fbaee0245977072a3b06ed94e2d3a47
MoggStephen <moggstephen66@gmail.com>
2023-03-24 16:36:18 +0000

    Initial commit

---
+++ notifications-data.js
@@ -0,0 +1,75 @@
+let notificationsData = {
+    "notifications-count": 3,
+    "notifications": [
+        {
+            "fullName": "Mark Webber",
+            "avatar": "./avatar-mark-webber.webp",
+            "event": {
+                "event": "react recent post",
+                "post": "My first tournament today!"
+            },
+            "timeSinceEventOccured": "1 mins",
+            "read": false
+        },
+        {
+            "name": "Angela Gray",
+            "avatar": "./avatar-angela-gray.webp",
+            "event": {
+                "event": "follow"
+            },
+            "timeSinceEventOccured": "5 mins",
+            "read": false
+        },
+        {
+            "name": "Jacob Thompson",
+            "avatar": "./avatar-jacob-thompson.webp",
+            "event": {
+                "event": "joined group",
+                "group": "Chess Club"
+            },
+            "timeSinceEventOccured": "1 days",
+            "read": false
+        },
+        {
+            "name": "Rizky Hasanuddin",
+            "avatar": "./avatar-rizky-hasanuddin.webp",
+            "event": {
+                "event": "private message",
+                "message": "Hello, thanks for setting up the Chess Club. I've been a member for a few weeks now and I'm already having lots of fun and improving my game."
+            },
+            "timeSinceEventOccured": "5 days",
+            "read": true
+        },
+        {
+            "name": "Kimberly Smith",
+            "avatar": "./avatar-kimberly-smith.webp",
+            "event": {
+                "event": "commented your picture",
+                "picture": "./assets/images/image-chess.webp"
+            },
+            "timeSinceEventOccured": "1 weeks",
+            "read": true
+        },
+        {
+            "name": "Nathan Peterson",
+            "avatar": "./avatar-nathan-peterson.webp",
+            "event": {
+                "event": "react recent post",
+                "post": "5 end-game strategies to increase your win rate"
+            },
+            "timeSinceEventOccured": "2 weeks",
+            "read": true
+        },
+        {
+            "name": "Anna Kim",
+            "avatar": "./avatar-anna-kim.webp",
+            "event": {
+                "event": "left group",
+                "group": "Chess Club"
+            },
+            "timeSinceEventOccured": "2 weeks",
+            "read": true
+        }
+    ]
+
+};
